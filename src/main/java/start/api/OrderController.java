df6c14237de921c58e529c62ba2553cb2b1db2ed
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-03-11 21:37:57 +0700

    add paypal

--- src/main/java/start/api/OrderController.java
+++ src/main/java/start/api/OrderController.java
@@ -4,25 +4,13 @@ package start.api;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import start.dto.request.RechargeRequestDTO;
-
 import start.service.WalletService;
-import start.utils.AccountUtils;
 import start.utils.ResponseHandler;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.UUID;
 
 @RestController
 @CrossOrigin("*")
@@ -36,9 +24,9 @@ public class OrderController {
     @Autowired
     WalletService walletService;
 
-    @PostMapping("/recharge")
+    @PostMapping("/request-recharge")
     public ResponseEntity createUrl(@RequestBody RechargeRequestDTO rechargeRequestDTO) throws Exception {
-       String url = walletService.createUrl(rechargeRequestDTO);
+       String url = walletService.createPaypalPayment(rechargeRequestDTO);
         return  responseHandler.response(200, "Create Url Successfully!", url);
     }
 
--- src/main/java/start/service/WalletService.java
+++ src/main/java/start/service/WalletService.java
@@ -1,8 +1,8 @@
 package start.service;
 
-
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import start.dto.request.RechargeRequestDTO;
 import start.entity.Transaction;
@@ -13,110 +13,50 @@ import start.repository.TransactionRepository;
 import start.repository.WalletRepository;
 import start.utils.AccountUtils;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.UUID;
-
 @Service
 public class WalletService {
 
     @Autowired
-    TransactionRepository transactionRepository;
+    private TransactionRepository transactionRepository;
 
     @Autowired
-    WalletRepository walletRepository;
+    private WalletRepository walletRepository;
+
     @Autowired
-    AccountUtils accountUtils;
+    private AccountUtils accountUtils;
 
+    @Autowired
+    private PaypalService payPalService;
 
-
-    public String createUrl(RechargeRequestDTO rechargeRequestDTO) throws NoSuchAlgorithmException, InvalidKeyException, Exception{
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmss");
-        LocalDateTime createDate = LocalDateTime.now();
-        String formattedCreateDate = createDate.format(formatter);
-
-
+    public String createPaypalPayment(RechargeRequestDTO rechargeRequestDTO) throws PayPalRESTException {
         User user = accountUtils.getCurrentUser();
-
-        String orderId = UUID.randomUUID().toString().substring(0,6);
 
         Wallet wallet = walletRepository.findWalletByUser_Id(user.getId());
 
         Transaction transaction = new Transaction();
-
         transaction.setAmount(Float.parseFloat(rechargeRequestDTO.getAmount()));
         transaction.setTransactionType(TransactionEnum.PENDING);
         transaction.setTo(wallet);
-        transaction.setTransactionDate(formattedCreateDate);
         transaction.setDescription("Recharge");
         Transaction transactionReturn = transactionRepository.save(transaction);
 
-        String tmnCode = "EDXJUBE1";
-        String secretKey = "AYBLLQTVRSRCZCGJGXJQCMNMLIYKKILF";
-        String vnpUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-        String returnUrl = "http://mycremo.art/checkout?id="+transactionReturn.getTransactionID();
+        Double totalAmount = Double.parseDouble(rechargeRequestDTO.getAmount());
+        String currency = "USD";
 
-        String currCode = "VND";
-        Map<String, String> vnpParams = new TreeMap<>();
-        vnpParams.put("vnp_Version", "2.1.0");
-        vnpParams.put("vnp_Command", "pay");
-        vnpParams.put("vnp_TmnCode", tmnCode);
-        vnpParams.put("vnp_Locale", "vn");
-        vnpParams.put("vnp_CurrCode", currCode);
-        vnpParams.put("vnp_TxnRef", orderId);
-        vnpParams.put("vnp_OrderInfo", "Thanh toan cho ma GD: " + orderId);
-        vnpParams.put("vnp_OrderType", "other");
-        vnpParams.put("vnp_Amount", rechargeRequestDTO.getAmount() +"00");
-        vnpParams.put("vnp_ReturnUrl", returnUrl);
-        vnpParams.put("vnp_CreateDate", formattedCreateDate);
-        vnpParams.put("vnp_IpAddr", "128.199.178.23");
+        String cancelUrl = "http://yourwebsite.com/cancel";
+        String successUrl = "http://mycremo.art/checkout?id="+transactionReturn.getTransactionID();
 
-        StringBuilder signDataBuilder = new StringBuilder();
-        for (Map.Entry<String, String> entry : vnpParams.entrySet()) {
-            signDataBuilder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.toString()));
-            signDataBuilder.append("=");
-            signDataBuilder.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.toString()));
-            signDataBuilder.append("&");
+        Payment payment = payPalService.createPayment(
+                totalAmount,
+                currency,
+                "Recharge",
+                cancelUrl,
+                successUrl);
+
+        if (payment != null) {
+            return payment.getLinks().get(1).getHref(); // Return the PayPal redirect URL
+        } else {
+            return null;
         }
-        signDataBuilder.deleteCharAt(signDataBuilder.length() - 1); // Remove last '&'
-
-        String signData = signDataBuilder.toString();
-        String signed = generateHMAC(secretKey, signData);
-
-        vnpParams.put("vnp_SecureHash", signed);
-
-        StringBuilder urlBuilder = new StringBuilder(vnpUrl);
-        urlBuilder.append("?");
-        for (Map.Entry<String, String> entry : vnpParams.entrySet()) {
-            urlBuilder.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.toString()));
-            urlBuilder.append("=");
-            urlBuilder.append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.toString()));
-            urlBuilder.append("&");
-        }
-        urlBuilder.deleteCharAt(urlBuilder.length() - 1); // Remove last '&'
-
-        return urlBuilder.toString();
     }
-
-    private String generateHMAC(String secretKey, String signData) throws NoSuchAlgorithmException, InvalidKeyException {
-        Mac hmacSha512 = Mac.getInstance("HmacSHA512");
-        SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "HmacSHA512");
-        hmacSha512.init(keySpec);
-        byte[] hmacBytes = hmacSha512.doFinal(signData.getBytes(StandardCharsets.UTF_8));
-
-        StringBuilder result = new StringBuilder();
-        for (byte b : hmacBytes) {
-            result.append(String.format("%02x", b));
-        }
-        return result.toString();
-    }
-
 }
