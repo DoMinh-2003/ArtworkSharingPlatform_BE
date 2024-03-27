f2c19116ed6470a28e39e2205ea15c39683721f8
DoMinh-2003 <minhlola28@gmail.com>
2024-03-28 01:10:27 +0700

    api get history buy artwork

--- src/main/java/start/service/TransactionService.java
+++ src/main/java/start/service/TransactionService.java
@@ -102,4 +102,38 @@ UserRepository userRepository;
 
         return listTransactionResponseDTO;
     }
+
+
+
+    public List<TransactionResponseDTO> historyBuyArtworkById() {
+        List<TransactionResponseDTO> listTransactionResponseDTO = new ArrayList<>();
+        User user = accountUtils.getCurrentUser();
+        Wallet wallet = walletRepository.findWalletByUser_Id(user.getId());
+        List<Transaction> transactions = transactionRepository.findTransactionsByFrom_IdOrTo_Id(wallet.getWalletID());
+        for (Transaction transaction : transactions) {
+            if(transaction.getArtworkID() != null) {
+                TransactionResponseDTO transactionResponseDTO = new TransactionResponseDTO();
+                transactionResponseDTO.setTransactionID(transaction.getTransactionID());
+                transactionResponseDTO.setTransactionType(transaction.getTransactionType());
+                transactionResponseDTO.setAmount(transaction.getAmount());
+                transactionResponseDTO.setDescription(transaction.getDescription());
+                transactionResponseDTO.setTransactionDate(transaction.getTransactionDate());
+                transactionResponseDTO.setArtwork(artworkRepository.findById((long) transaction.getArtworkID()));
+                transactionResponseDTO.setFrom(transaction.getFrom());
+                transactionResponseDTO.setTo(transaction.getTo());
+                if (transaction.getFrom() != null) {
+                    transactionResponseDTO.setUserFrom(transaction.getFrom().getUser());
+                }
+                if (transaction.getTo() != null) {
+                    transactionResponseDTO.setUserTo(transaction.getTo().getUser());
+                }
+                listTransactionResponseDTO.add(transactionResponseDTO);
+            }
+        }
+        listTransactionResponseDTO = listTransactionResponseDTO.stream()
+                .sorted(Comparator.comparing(TransactionResponseDTO::getTransactionDate).reversed())
+                .collect(Collectors.toList());
+
+        return listTransactionResponseDTO;
+    }
 }
